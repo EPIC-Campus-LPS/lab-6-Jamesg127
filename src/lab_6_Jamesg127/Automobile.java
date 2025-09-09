cd8814d4f7302c43010f3f53ff272b7877866de5
Jamesg127 <jamesg127@lpsk12.org>
2025-09-09 10:59:02 -0600

    Je suis un croissant

---
+++ src/lab_6_Jamesg127/Automobile.java
@@ -0,0 +1,29 @@
+package lab_6_Jamesg127;
+
+public class Automobile {
+	private String make;
+	private String model;
+	private double mileage;
+	private double fuelTank;
+	private double fuel;
+	Automobile(String mk, String mdl, double m, double fT){
+		make = mk;
+		model = mdl;
+		mileage = m;
+		fuelTank = fT;
+	}
+	public void fillUp(double gallons){
+		if (fuel + gallons <= fuelTank) {
+			fuel = gallons + fuel;
+		}
+		else {
+			fuel = fuelTank;
+		}
+	}
+	public void takeTrip(double miles) {
+		fuel = fuelTank - miles / mileage;
+	}
+	public double reportFuel() {
+		return fuel;
+	}
+}
---
+++ src/lab_6_Jamesg127/ReadFromFileUsingScanner.java
@@ -0,0 +1,15 @@
+package lab_6_Jamesg127;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+public class ReadFromFileUsingScanner {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		File file = new File("park.txt");
+		Scanner sc = new Scanner(file);
+		while(sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}	
+	}
+}
